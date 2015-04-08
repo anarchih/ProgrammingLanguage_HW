583f4279aa903e3c930106bec4c9eefcfe783d77
anarchih <anarchih.tw@gmail.com>
2015-04-08 09:13:22 +0800

    Successfully load web page and parse it

--- test.pl
+++ test.pl
@@ -1,14 +1,25 @@
-open(my $str, '<:encoding(UTF-8)', "test.html");
-
-
-$row = <$str>;
+use LWP::Simple;
+use Encode;
+use utf8;
+use open ':std', ':encoding(UTF-8)';
+$url = "http://www.atmovies.com.tw/showtime/theater_t06608_a06.html";
+getstore($url, 'download.html');
+open(my $str, '<:encoding(UTF-8)', "download.html");
 $i = 0;
 while (my $row = <$str>){
-    # if ($i > 20){
-    #    last;
-    # }
     $i += 1;
-    if($row =~ /\s{10,100}<a\shref=".+>.*<\/a>\s+/){
-        print $row;
+    # for name
+    if($row =~ /(\s{10,100}<a\shref=".+>.*<\/a>\s+)/){
+        print $row =~ /.*>(.*)<.*/;
+        print "\n";
+        
+    }
+    # print $row;
+    # for time
+    if($row =~ /\s+(<LI>.+<\/LI>)+/){
+        my @test = $row =~ /(\d+：\d+)/g;
+        foreach (@test) {
+              print "$_\n";
+        }
     }
 }
